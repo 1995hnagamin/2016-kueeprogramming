b5d492d353ca0aa565fdd4418931bf6710c50b8c
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2016-10-06 11:53:53 +0900

    [WIP] Prepare matrix library
    Matrix, Vector manages memory.
    
    Ref: http://tondol.hatenablog.jp/entry/20090713/1247426321

--- problem1/matrix.c
+++ problem1/matrix.c
@@ -5,41 +5,92 @@
 
 typedef double Element;
 
-#define N 4
-
 typedef struct {
   size_t row;
   size_t column;
-  Element *ptr;
+  Element **ptr;
 } Matrix;
 
 Matrix mat_alloc(size_t m, size_t n) {
-  Element *ptr = (Element*)malloc(sizeof(Element) * m * n);
-  assert(ptr);
-  Matrix mat = { m, n, ptr };
+  Element *base_ptr = malloc(sizeof(Element) * m * n);
+  assert(base_ptr);
+  Element **rows = malloc(sizeof(Element*) * m);
+  assert(rows);
+  for (size_t i = 0; i < m; ++i) {
+    rows[i] = base_ptr + i * m;
+  }
+  Matrix mat = { m, n, rows };
   return mat;
 }
 
 void mat_free(Matrix mat) {
+  free(mat.ptr[0]);
   free(mat.ptr);
 }
 
-Matrix mat_init(size_t m, size_t n, Element const **arr) {
+Matrix mat_init(size_t m, size_t n, Element *arr) {
   Matrix mat = mat_alloc(m, n);
   for (int j = 0; j < n; ++j) {
     for (int i = 0; i < m; ++i) {
-      mat.ptr[i * m + j] = arr[i][j];
+      mat.ptr[i][j] = arr[i * m + j];
     }
   }
   return mat;
 }
+#define MAT_INIT(m, n, arr) mat_init(m, n, &(arr[0][0]))
+
+typedef struct {
+  size_t size;
+  Element *ptr;
+} Vector;
+
+Vector vec_alloc(size_t size) {
+  Element *ptr = malloc(sizeof(Element) * size);
+  assert(ptr);
+  Vector vec = { size, ptr };
+  return vec;
+}
+
+Vector vec_free(Vector vec) {
+  free(vec.ptr);
+}
+
+
+
+Vector mult_mat_by_vec(Matrix m, Vector v) {
+  Vector value = vec_alloc(v.size);
+  for (size_t i = 0; i < m.row; ++i) {
+    Element sum = 0;
+    for (size_t j = 0; j < m.column; ++j) {
+      sum += m.ptr[i][j] * v.ptr[j];
+    }
+    value.ptr[i] = sum;
+  }
+  return value;
+}
+
+Matrix mult_mat_by_mat(Matrix a, Matrix b) {
+  assert();
+  Matrix value = mat_alloc( , );
+  for (size_t i = 0; i < ; ++i) {
+    for (size_t j = 0; j < ; ++j) {
+      Element sum = 0;
+      for (size_t k = 0; k < ; ++k) {
+        sum += a.ptr[i][k] * b.ptr[k][j];
+      }
+      value.ptr[i][j] = sum;
+    }
+  }
+  return value;
+}
 
 int main() {
-  Element mat_v[4][4] = {
+  Element mat_v[5][5] = {
     { 1, 2, 3, 4 },
     { 5, 6, 7, 8 },
-    { 9, 10, 11, 12 }
+    { 9, 10, 11, 12 },
+    { 13, 14, 15, 16 }
   };
-  Matrix mat = mat_init(N, N, mat_v);
+  Matrix mat = MAT_INIT(5, 5, mat_v);
   return 0;
 }
