c47c4fee099c567fc805cc347ea786f13eec055f
Hideaki Nagamine <1995.hnagamin@gmail.com>
2016-12-18 18:31:58 +0900

    Define Util.simulate_with_euler

--- problem3/prob1.ml
+++ problem3/prob1.ml
@@ -1,15 +1,3 @@
-let euler f = Util.calcSSM f
-
 let _ =
   let prob1 _ (x, y, vx, vy) = (vx, vy, y, -1.0 *. x) in
-  let dt = 2.0 *. Util.pi /. 64.0 in
-  let print (n, state) =
-    let (x, y, _, _) = state in
-    let t = (float_of_int n) *. dt in
-    let error = Util.abs_vector2
-      (Util.minus_vector2 (x, y) (-1.0 *. cos(t), sin(t))) in
-    Printf.printf "%f\t%f\t%f\t%f\n" t x y error
-  in
-  let result = euler prob1 dt 320 (0, (-1.0, 0.0, 0.0, 1.0)) in
-  print_string "#t x y error\n";
-  List.iter print result
+  Util.simulate_with_euler prob1 64
--- problem3/util.ml
+++ problem3/util.ml
@@ -26,6 +26,17 @@ let abs_vector2 (a, b) =
 
 let pi = 4.0 *. atan 1.0
 
+let power_int a n =
+  let rec f b m =
+    if m = 0 then 1
+    else if m mod 2 = 0 then f (b * b) (m / 2)
+    else b * (f b (m - 1))
+  in
+  if n >= 0 then
+    f a n
+  else
+    raise (invalid_arg "negative power")
+
 let expand f init size =
   let rec loop lst i =
     match i with
@@ -51,3 +62,18 @@ let calcSSM phi h steps (t0, y0) =
     y +: (h *: (phi t y))
   in
   expand1 (fun (n, y) -> (n + 1, develop (n, y))) [(t0, y0)] steps
+
+let euler f = calcSSM f
+
+let simulate_with_euler f scale =
+  let dt = 2.0 *. pi /. (float_of_int scale) in
+  let print (n, state) =
+    let (x, y, _, _) = state in
+    let t = (float_of_int n) *. dt in
+    let error = abs_vector2
+      (minus_vector2 (x, y) (-1.0 *. cos(t), sin(t))) in
+    Printf.printf "%f\t%f\t%f\t%f\n" t x y error
+  in
+  let result = euler f dt (scale * 5) (0, (-1.0, 0.0, 0.0, 1.0)) in
+  print_string "#t x y error\n";
+  List.iter print result
