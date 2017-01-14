9e27129154676a168bbb0d8f2f9fd86c5bd0967f
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-01-14 22:21:12 +0900

    Implement Power Method
    Add do_power_method(Matrix, Vector, double).

--- problem4/prob1.c
+++ problem4/prob1.c
@@ -5,6 +5,11 @@
 #include "matrix.h"
 #include "lu.h"
 
+typedef struct {
+  double value;
+  Vector vector;
+} EigenPair;
+
 double dissimilarity(Vector numer, Vector denom) {
   assert(numer.size == denom.size);
   size_t const n = numer.size;
@@ -15,6 +20,30 @@ double dissimilarity(Vector numer, Vector denom) {
     r_min = fmin(r_min, r), r_max = fmax(r_max, r);
   }
   return fabs(r_max - r_min);
+}
+
+EigenPair do_power_method(Matrix A, Vector init, double epsilon) {
+  Vector x = vec_copy(init);
+  normalize_vec_dstly(&x);
+
+  double rayleigh_quo;
+  while (1) {
+    Vector xnew = mult_mat_by_vec(A, x);
+    normalize_vec_dstly(&xnew);
+
+    rayleigh_quo = dot_product(x, xnew) / dot_product(x, x);
+    double dissim = dissimilarity(x, xnew);
+
+    vec_free(x);
+    x = xnew;
+
+    if (dissim < epsilon) {
+      break;
+    }
+  }
+
+  EigenPair p = { rayleigh_quo, x };
+  return p;
 }
 
 int main(void) {
