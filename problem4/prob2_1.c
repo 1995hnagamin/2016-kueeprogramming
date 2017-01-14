464ac05fcf8b5e7bbe307a9ed92879b6061141b7
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-01-14 21:41:49 +0900

    Use trapezoidal_integral in prob2_1

--- problem4/prob2_1.c
+++ problem4/prob2_1.c
@@ -5,5 +5,11 @@
 typedef double (*Func)(double);
 
 int main(void) {
+  int const limit = 10;
+  for (int k = 0; k <= limit; ++k) {
+    double const answer = 1.0 / 20;
+    double const approx = trapezoidal_integral(f, 0, 1, k);
+    printf("%d %.10lf\n", k, log2(fabs(answer - approx)));
+  }
   return 0;
 }
