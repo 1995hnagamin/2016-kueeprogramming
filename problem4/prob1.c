77a6cadda7cff7bd0e312da0065b40a49c4c47ab
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-01-14 22:20:17 +0900

    Add helper function dissimilarity(Vector, Vector)

--- problem4/prob1.c
+++ problem4/prob1.c
@@ -5,6 +5,18 @@
 #include "matrix.h"
 #include "lu.h"
 
+double dissimilarity(Vector numer, Vector denom) {
+  assert(numer.size == denom.size);
+  size_t const n = numer.size;
+
+  double r_min, r_max; r_min = r_max = numer.ptr[0] / denom.ptr[0];
+  for (size_t i = 1; i < n; ++i) {
+    double const r = numer.ptr[i] / denom.ptr[i];
+    r_min = fmin(r_min, r), r_max = fmax(r_max, r);
+  }
+  return fabs(r_max - r_min);
+}
+
 int main(void) {
   return 0;
 }
