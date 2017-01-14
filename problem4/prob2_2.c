d7825a4fc70d9f46627f73000f921172bd921406
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-01-14 21:47:16 +0900

    Add prob2_2

---
+++ problem4/prob2_2.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+#include "mathutil.h"
+#include "util2.h"
+
+int main(void) {
+  int const limit = 10;
+  for (int k = 1; k <= limit; ++k) {
+    double const answer = 1.0 / 20;
+    double const approx = simpson_integral(f, 0, 1, k);
+    printf("%d %.10lf\n", k, log2(fabs(answer - approx)));
+  }
+  return 0;
+}
