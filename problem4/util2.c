b88e7e3011ac1c1644d2dd9fea49176b9e780929
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-01-14 21:24:15 +0900

    Add /problem4/util2.{c, h}

---
+++ problem4/util2.c
@@ -0,0 +1,7 @@
+#include <math.h>
+#include "util2.h"
+
+double f(double x) {
+  return pow(x, 19);
+}
+
---
+++ problem4/util2.h
@@ -0,0 +1,8 @@
+#ifndef UTIL2_H
+#define UTIL2_H
+
+typedef double (*Func)(double);
+
+double f(double);
+
+#endif
