b4e24c91c028fc93c5677d7da1d178b6ddae8e2f
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-01-29 17:34:14 +0900

    Output log_2(E_r) in problem 3.2

--- problem3/prob2.c
+++ problem3/prob2.c
@@ -6,7 +6,7 @@
 int main(void) {
   for (int p = 3; p <= 18; ++p) {
     double const error = calc_max_error(powi(2, p), step_euler);
-    printf("%d\t%lf\n", p, error);
+    printf("%d\t%lf\t%lf\n", p, error, log2(error));
   }
   return 0;
 }
